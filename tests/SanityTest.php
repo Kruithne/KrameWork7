203fdaf6df55df16bc46fc89490b1da188f09132
Kruithne <kruithne@gmail.com>
2017-01-24 12:19:33 +0000

    Unit testing; for science!

---
+++ tests/SanityTest.php
@@ -0,0 +1,13 @@
+<?php
+	class SanityTest extends PHPUnit_Framework_TestCase
+	{
+		/**
+		 * Check if the boolean value of true, is equal to itself.
+		 */
+		public function testSanity()
+		{
+			// If this fails, abandon all hope.
+			$this->assertEquals(true, true, "The world as we once knew it, has ended.");
+		}
+	}
+?>
