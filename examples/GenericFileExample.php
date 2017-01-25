58c1408c4eda2145199b9db67a4261173e916b03
Kruithne <kruithne@gmail.com>
2017-01-25 13:33:46 +0000

    Include code examples for GenericFile, JSONFile and KeyValueContainer.

---
+++ examples/GenericFileExample.php
@@ -0,0 +1,31 @@
+<?php
+	namespace KrameWork\Storage;
+
+	// Create a file wrapper and read from disk.
+	// Will throw KrameWorkFileException if the file does not exist or cannot be read.
+	$file = new GenericFile("myFile.txt");
+
+	// Access the loaded data.
+	print($file->getData());
+
+	// Create a file wrapper without reading from disk.
+	$file = new GenericFile("myFile.txt", false);
+
+	// Check if the file actually exists before reading.
+	if ($file->exists())
+		$file->read(); // Read file data into the wrapper.
+
+	// Overwrite the data.
+	$file->setData("New data!");
+
+	// Save to myFile.txt
+	$file->save();
+
+	// Create a file wrapper without a file.
+	$file = new GenericFile();
+
+	// Write some data to the file.
+	$file->setData("Hello, world!");
+
+	// Save to a specific file.
+	$file->save("anotherFile.txt");
---
+++ examples/JSONFileExample.php
@@ -0,0 +1,21 @@
+<?php
+	namespace KrameWork\Storage;
+
+	// Inherits functionality from BaseFile and uses an internal KeyValueContainer.
+	// Check examples/documentation on those for more in-depth information.
+
+	// Load a JSON file. By default, this will use an internal KeyValueContainer to store it.
+	// Will throw a KrameWorkFileException if the file does not exist, or cannot be read.
+	$file = new JSONFile("bigData.json");
+
+	// Set options for this wrapper.
+	$file->setOptions(JSON_BIGINT_AS_STRING);
+
+	// Access JSON values.
+	print($file->myValue);
+
+	// Assign values.
+	$file->myValue = 42;
+
+	// Save to file encoded as JSON.
+	$file->save();
