8c25102f9366c0dfc9f216d11f538b184fc5586b
Kruithne <kruithne@gmail.com>
2017-01-26 13:56:43 +0000

    Added path utility class with unit test.

---
+++ src/Utils/PathUtil.php
@@ -0,0 +1,13 @@
+<?php
+	namespace KrameWork\Utils;
+
+	class PathUtil {
+		/**
+		 * Convert all slashes in a string to the current environment standard.
+		 * @param string $path
+		 * @return string
+		 */
+		static function formatSlashes(string $path):string {
+			return str_replace(DIRECTORY_SEPARATOR == "/" ? "\\" : "/", DIRECTORY_SEPARATOR, $path);
+		}
+	}
