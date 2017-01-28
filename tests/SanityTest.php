f732adf78648a7607f1317f95ee4b24cdb0ed698
Kruithne <kruithne@gmail.com>
2017-01-28 15:56:29 +0000

    Fix bracing style in SanityTest.

--- tests/SanityTest.php
+++ tests/SanityTest.php
@@ -4,8 +4,7 @@
 		/**
 		 * Check if the boolean value of true, is equal to itself.
 		 */
-		public function testSanity()
-		{
+		public function testSanity() {
 			// If this fails, abandon all hope.
 			$this->assertEquals(true, true, "The world as we once knew it, has ended.");
 		}
