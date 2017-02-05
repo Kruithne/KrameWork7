4b1e3d5549a39be31a7c9bcd5984fbb31faef4ec
Kruithne <kruithne@gmail.com>
2017-02-05 23:37:34 +0000

    Added StringBuilder class with unit test.

---
+++ src/Utils/StringBuilder.php
@@ -0,0 +1,104 @@
+<?php
+	namespace KrameWork\Utils;
+
+	/**
+	 * Class StringBuilder
+	 * Fluent API String Builder.
+	 *
+	 * @package KrameWork\Utils
+	 * @author Kruithne (kruithne@gmail.com)
+	 */
+	class StringBuilder
+	{
+		/**
+		 * StringBuilder constructor.
+		 * Accepts variable arguments of strings, arrays and objects.
+		 *
+		 * @api
+		 */
+		public function __construct() {
+			$this->clear();
+			$this->append(func_get_args());
+		}
+
+		/**
+		 * Append string(s) to the builder.
+		 * Accepts variable arguments of strings, arrays or objects.
+		 *
+		 * @api
+		 * @return StringBuilder
+		 */
+		public function append():StringBuilder {
+			foreach (func_get_args() as $arg) {
+				if (is_array($arg)) {
+					foreach ($arg as $subArg)
+						$this->append($subArg);
+				} else {
+					$this->data .= strval($arg);
+				}
+			}
+			return $this;
+		}
+
+		/**
+		 * Prepend string(s) to the builder.
+		 * Accepts variable arguments of strings, arrays or objects.
+		 *
+		 * @api
+		 * @return StringBuilder
+		 */
+		public function prepend():StringBuilder {
+			foreach (func_get_args() as $arg) {
+				if (is_array($arg)) {
+					foreach ($arg as $subArg)
+						$this->prepend($subArg);
+				} else {
+					$this->data = strval($arg) . $this->data;
+				}
+			}
+			return $this;
+		}
+
+		/**
+		 * Append/prepend a string $count amount of times.
+		 *
+		 * @api
+		 * @param mixed $input Object that can be cast to a string.
+		 * @param int $count How many times to append/prepend the string.
+		 * @param bool $append True = Append, False = Prepend.
+		 * @return StringBuilder
+		 */
+		public function repeat($input, $count = 1, $append = true):StringBuilder {
+			$line = str_repeat(strval($input), $count);
+			$append ? $this->append($line) : $this->prepend($line);
+
+			return $this;
+		}
+
+		/**
+		 * Return the compiled result of the string builder.
+		 *
+		 * @return string
+		 * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.tostring
+		 */
+		function __toString() {
+			return $this->data;
+		}
+
+
+		/**
+		 * Clear any input data to the string builder.
+		 *
+		 * @api
+		 * @return StringBuilder
+		 */
+		public function clear():StringBuilder {
+			$this->data = "";
+			return $this;
+		}
+
+		/**
+		 * @var string
+		 */
+		private $data;
+	}
---
+++ tests/StringBuilderTest.php
@@ -0,0 +1,95 @@
+<?php
+	use KrameWork\Utils\StringBuilder;
+
+	require_once(__DIR__ . "/../src/Utils/StringBuilder.php");
+
+	class StringBuilderTest extends \PHPUnit_Framework_TestCase
+	{
+		/**
+		 * Test the default internal string of the builder.
+		 */
+		public function testDefault() {
+			$builder = new StringBuilder();
+			$this->assertEquals("", $builder->__toString());
+			unset($builder);
+		}
+
+		/**
+		 * Test the StringBuilder append() functionality.
+		 */
+		public function testAppend() {
+			$builder = new StringBuilder();
+			$builder->append("Hello")->append("World");
+
+			$this->assertEquals("HelloWorld", $builder->__toString());
+			unset($builder);
+		}
+
+		/**
+		 * Test appending objects to the StringBuilder.
+		 */
+		public function testAppendObject() {
+			$obj = new class { public function __toString() { return "Boots"; } };
+			$builder = new StringBuilder();
+			$builder->append("Cats", $obj);
+
+			$this->assertEquals("CatsBoots", $builder->__toString());
+			unset($builder, $obj);
+		}
+
+		/**
+		 * Test appending arrays to the StringBuilder.
+		 */
+		public function testAppendArray() {
+			$arr = ["Hairy", "Bees"];
+			$builder = new StringBuilder();
+			$builder->append($arr, "Help");
+
+			$this->assertEquals("HairyBeesHelp", $builder->__toString());
+			unset($builder, $arr);
+		}
+
+		/**
+		 * Test the StringBuilder prepend() functionality.
+		 */
+		public function testPrepend() {
+			$builder = new StringBuilder();
+			$builder->prepend("Hello")->prepend("World");
+
+			$this->assertEquals("WorldHello", $builder->__toString());
+			unset($builder);
+		}
+
+		/**
+		 * Test the StringBuilder repeat() append functionality.
+		 */
+		public function testRepeatAppend() {
+			$builder = new StringBuilder();
+			$builder->append("Start")->repeat("Test", 3, true);
+
+			$this->assertEquals("StartTestTestTest", $builder->__toString());
+			unset($builder);
+		}
+
+		/**
+		 * Test the StringBuilder repeat() prepend functionality.
+		 */
+		public function testRepeatPrepend() {
+			$builder = new StringBuilder();
+			$builder->append("End")->repeat("Test", 3, false);
+
+			$this->assertEquals("TestTestTestEnd", $builder->__toString());
+			unset($builder);
+		}
+
+		/**
+		 * Test the initial value of the StringBuilder.
+		 */
+		public function testInitialValue() {
+			$builder = new StringBuilder("Hello");
+			$builder->append("World");
+
+			$this->assertEquals("HelloWorld", $builder->__toString());
+			unset($builder);
+		}
+	}
