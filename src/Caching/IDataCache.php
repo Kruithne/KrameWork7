7fc5d3e647e4edf78fcd65521a73141df6651a9e
Kruithne <kruithne@gmail.com>
2017-02-14 12:23:30 +0000

    Fix issue with tests expecting 'void' return type.

--- src/Caching/IDataCache.php
+++ src/Caching/IDataCache.php
@@ -51,7 +51,7 @@
 		 * @param string $key Key to store the value under.
 		 * @param mixed $value Value to store in the cache.
 		 */
-		public function __set(string $key, $value):void;
+		public function __set(string $key, $value);
 
 		/**
 		 * Store a value in the cache.
@@ -61,7 +61,7 @@
 		 * @param mixed $value Value to store in the cache.
 		 * @param int $expire 60*60*24*30 >= Unix Timestamp, otherwise seconds. 0 = Never.
 		 */
-		public function store(string $key, $value, int $expire = 0):void;
+		public function store(string $key, $value, int $expire = 0);
 
 		/**
 		 * Check if a value exists in the cache.
@@ -77,7 +77,7 @@
 		 * @api __unset
 		 * @param string $key Key of the item to remove.
 		 */
-		public function __unset(string $key):void;
+		public function __unset(string $key);
 
 		/**
 		 * Flush the cache, removing all stored data.
@@ -93,7 +93,7 @@
 		 * @param string $key Key of the value.
 		 * @param int $weight How much to increment the value.
 		 */
-		public function increment(string $key, int $weight):void;
+		public function increment(string $key, int $weight);
 
 		/**
 		 * Decrease a numeric value in the cache.
