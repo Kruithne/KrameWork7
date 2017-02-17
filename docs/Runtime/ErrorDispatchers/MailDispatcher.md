fdc3f80f2fbca950d43070564d5be71b93c3b553
Kruithne <kruithne@gmail.com>
2017-02-17 22:37:06 +0000

    Add error dispatcher: MailDispatcher. Closes #35.

---
+++ docs/Runtime/ErrorDispatchers/MailDispatcher.md
@@ -0,0 +1,45 @@
+## KrameWork\Runtime\ErrorDispatchers\MailDispatcher
+
+***Table of Contents***
+* **Overview** - Information about the class.
+* **Example** - Example usage.
+* **Functions** - Comprehensive list of all functions in the class.
+
+___
+### Overview
+The `MailDispatcher` class is an implementation of `IErrorDispatcher` for use with the `ErrorHandler` class. If using this class as the error dispatcher, reports will be mailed to the given recipients. The name of the report (used as the subject) can be controlled with the second parameter as either a string or a callable generator.
+
+___
+### Example
+This is a use-case example for this dispatcher, using a naming callback instead of a static string.
+```php
+function createLogName():string {
+    return 'Error Report: ' . md5(time() + mt_rand());
+}
+
+$dispatcher = new MailDispatcher(
+    ['foo@bar.net' => 'Foo Bar'], // Recipient array.
+    'error-reporting@bar.net', // Sender address.
+    'Error Reporter', // Sender name.
+    ['createLogName'] // Report/subject name generator/string.
+);
+// Provide $dispatcher to an instance of ErrorHandler.
+```
+___
+### Functions
+##### > __construct() : `void`
+MailDispatcher constructor.
+
+parameter | type | description
+--- | --- |---
+`$recipients` | `array` | Recipient array, formatted in accordance to the `Mail` class.
+`$sender` | `string` | E-mail address the error reports are sent from.
+`$senderName` | `string` | Name of the e-mail sender, defaults to `$sender`.
+`$subject` | `string|array` | Subject/report name string or generator.
+
+##### > dispatch() : `void`
+Dispatch an error report.
+
+parameter | type | description
+--- | --- | ---
+`$report` | `string` | Report to dispatch.
---
+++ src/Runtime/ErrorDispatchers/MailDispatcher.php
@@ -0,0 +1,86 @@
+<?php
+	/*
+	 * Copyright (c) 2017 Kruithne (kruithne@gmail.com)
+	 * https://github.com/Kruithne/KrameWork7
+	 *
+	 * Permission is hereby granted, free of charge, to any person obtaining a copy
+	 * of this software and associated documentation files (the "Software"), to deal
+	 * in the Software without restriction, including without limitation the rights
+	 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	 * copies of the Software, and to permit persons to whom the Software is
+	 * furnished to do so, subject to the following conditions:
+	 *
+	 * The above copyright notice and this permission notice shall be included in all
+	 * copies or substantial portions of the Software.
+	 *
+	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	 * SOFTWARE.
+	 */
+
+	namespace KrameWork\Runtime\ErrorDispatchers;
+
+	use KrameWork\Mailing\Mail;
+	use KrameWork\Runtime\ErrorFormatters\IErrorFormatter;
+
+	require_once(__DIR__ . '/../../Mailing/Mail.php');
+
+	/**
+	 * Class MailDispatcher
+	 * Dispatches error reports via e-mail.
+	 *
+	 * @package KrameWork\Runtime\ErrorDispatchers
+	 * @author Kruithne <kruithne@gmail.com>
+	 */
+	class MailDispatcher implements IErrorDispatcher
+	{
+		/**
+		 * MailDispatcher constructor.
+		 *
+		 * @api __construct
+		 * @param array $recipients Recipients to receive this report.
+		 * @param string $sender Sender e-mail address.
+		 * @param string $senderName Name of the e-mail sender.
+		 * @param string|array $subject Subject string or callable generator.
+		 */
+		public function __construct(array $recipients, string $sender, string $senderName = null, $subject = 'Error Report') {
+			$this->mail = new Mail();
+			$this->mail->to->add($recipients);
+			$this->mail->setSender($sender, $senderName, true);
+
+			if (!is_array($subject))
+				$this->mail->setSubject($subject);
+			else
+				$this->subjectGen = $subject;
+		}
+
+		/**
+		 * Dispatch an error report.
+		 *
+		 * @api dispatch
+		 * @param IErrorFormatter|string $report Report to dispatch.
+		 */
+		public function dispatch($report) {
+			if ($this->subjectGen) {
+				$subject = call_user_func(count($this->subjectGen) == 1 ? $this->subjectGen[0] : $this->subjectGen);
+				$this->mail->setSubject($subject);
+			}
+
+			$this->mail->htmlContent->setContent($report);
+			$this->mail->send();
+		}
+
+		/**
+		 * @var Mail
+		 */
+		protected $mail;
+
+		/**
+		 * @var array
+		 */
+		protected $subjectGen;
+	}
