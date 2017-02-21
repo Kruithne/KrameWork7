8de6c46716273127b346ab947c863655d51f003c
Kruithne <kruithne@gmail.com>
2017-02-21 01:14:42 +0000

    Added v4 pseudo-random UUID generator.

--- src/Utils/UUID.php
+++ src/Utils/UUID.php
@@ -47,6 +47,22 @@
 		}
 
 		/**
+		 * Generate an RFC 4211 compliant v4 UUID.
+		 *
+		 * @api generate_v4
+		 * @return string
+		 */
+		public static function generate_v4():string {
+			return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+				mt_rand(0, 0xffff), mt_rand(0, 0xffff), // 32 bits for "time_low"
+				mt_rand(0, 0xffff), // 16 bits for "time_mid"
+				mt_rand(0, 0x0fff) | 0x4000, // 16 bits for "time_hi_and_version", four most significant bits holds version number 4
+				mt_rand(0, 0x3fff) | 0x8000, // 16 bits, 8 bits for "clk_seq_hi_res", 8 bits for "clk_seq_low", two most significant bits holds zero and one for variant DCE1.1
+				mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff) // 48 bits for "node"
+			);
+		}
+
+		/**
 		 * Generate an RFC 4211 compliant v5 UUID.
 		 * Returns false when given an invalid namespace.
 		 *
