55aedfaa92776dc3a4a9c0dc1284913cec18e7e0
Kruithne <kruithne@gmail.com>
2017-02-26 23:23:52 +0000

    Add SecurityPolicy class to encompass security headers.

---
+++ docs/Security/HTTP/SecurityPolicy.md
@@ -0,0 +1,58 @@
+## KrameWork\Security\HTTP\SecurityPolicy
+
+***Table of Contents***
+* **Overview** - Information about the class.
+* **Example** - Example usage.
+* **Functions** - Comprehensive list of all functions in the class.
+
+___
+### Overview
+The `SecurityPolicy` class is intended to encompass control and generation of commonly used security headers. The object provides a `compile()` function, which should be called before caching the object. Converting the object to a string (`__toString()`) will also return an encoded version after compilation, which can be fed into the constructor to skip the need to compile/provide headers.
+___
+### Example
+Here is a basic example of how to use this class. It's worth nothing this example doesn't delve deep into methods of caching, since it may differ on your application.
+```php
+$policy = new SecurityPolicy();
+$policy->add(new XSSProtectionHeader());
+$policy->add(new MIMEValidationHeader());
+$policy->add(new XFrameHeader(XFrameHeader::DENY));
+$policy->add(new HSTSHeader(63072000, true, true));
+$policy->add(new CSPHeader([
+    CSPHeader::DIRECTIVE_DEFAULT => [CSPHeader::SOURCE_HTTPS, CSPHeader::SOURCE_SELF],
+    CSPHeader::DIRECTIVE_SCRIPT => ['https://cdnjs.cloudflare.com', 'https://www.google.com/'],
+    CSPHeader::DIRECTIVE_FONT => 'https://fonts.googleapis.com/',
+    CSPHeader::DIRECTIVE_STYLE => [CSPHeader::SOURCE_INLINE, 'https://cdnjs.cloudflare.com/'],
+    CSPHeader::DIRECTIVE_OBJECT => CSPHeader::SOURCE_NONE,
+    CSPHeader::DIRECTIVE_CHILD => CSPHeader::SOURCE_NONE
+]));
+$policy->compile();
+
+// Now that the policy is compiled, you can (see: should) cache either the instance, or the result of `__toString()` which can be passed into the constructor of the class and skipping everything above.
+
+$policy->apply(); // Set all HTTP headers with the policy set-up.
+```
+___
+### Functions
+##### > __construct() : `void`
+SecurityPolicy constructor.
+
+parameter | type | description
+--- | --- | ---
+`$input` | `array|string` | Policy array (header objects) or pre-compiled string.
+
+##### > add() : `void`
+Add a header to this policy.
+
+parameter | type | description
+--- | --- | ---
+`$header` | `HTTPHeader` | Header to add.
+
+##### > apply() : `void`
+Apply this policy, compiling and setting all headers. Unless providing a pre-compiled string, call compile() first.
+
+##### > compile() : `void`
+Compiled all headers in this policy, storing them internally. Call before applying a non pre-compiled policy.
+
+##### > __toString() : `void`
+Get a serialized representation of this policy. Results may be incorrect if called before compile().
+
---
+++ src/Security/HTTP/SecurityPolicy.php
@@ -0,0 +1,112 @@
+<?php
+	/*
+	 * Copyright (c) 2017 Kruithne (kruithne@gmail.com)
+	 * https://github.com/Kruithne/KrameWork7
+	 *
+	 * Permission is hereby granted, free of charge, to any person obtaining a copy
+	 * of this software and associated documentation files (the "Software"), to deal
+	 * in the Software without restriction, including without limitation the rights
+	 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	 * copies of the Software, and to permit persons to whom the Software is
+	 * furnished to do so, subject to the following conditions:
+	 *
+	 * The above copyright notice and this permission notice shall be included in all
+	 * copies or substantial portions of the Software.
+	 *
+	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	 * SOFTWARE.
+	 */
+	namespace KrameWork\Security\HTTP;
+	require_once(__DIR__ . '/HTTPHeader.php');
+
+	/**
+	 * Class SecurityPolicy
+	 * Application security control module.
+	 *
+	 * @package KrameWork\Security
+	 * @author Kruithne <kruithne@gmail.com>
+	 */
+	class SecurityPolicy
+	{
+		/**
+		 * SecurityPolicy constructor.
+		 *
+		 * @api __construct
+		 * @param array|string $input Policy array (header objects) or pre-compiled string.
+		 */
+		public function __construct($input = []) {
+			$this->compiled = [];
+
+			if (is_array($input)) {
+				$this->headers = $input;
+			} else {
+				$this->headers = [];
+
+				$decode = base64_decode($input);
+				if ($decode !== false) {
+					$parsed = json_decode($decode);
+					if ($parsed !== null)
+						$this->compiled = $parsed;
+				}
+			}
+		}
+
+		/**
+		 * Add a header to this policy.
+		 *
+		 * @api add
+		 * @param HTTPHeader $header Header to add.
+		 */
+		public function add(HTTPHeader $header) {
+			$this->headers[] = $header;
+		}
+
+		/**
+		 * Apply this policy, compiling and setting all headers.
+		 * Unless providing a pre-compiled string, call compile() first.
+		 *
+		 * @api apply
+		 */
+		public function apply() {
+			foreach ($this->compiled as $header)
+				var_dump($header);
+		}
+
+		/**
+		 * Compiled all headers in this policy, storing them internally.
+		 * Call before applying a non pre-compiled policy.
+		 *
+		 * @api compile
+		 */
+		public function compile() {
+			$this->compiled = [];
+			foreach ($this->headers as $header)
+				$this->compiled[] = $header->__toString();
+		}
+
+		/**
+		 * Get a serialized representation of this policy.
+		 * Results may be incorrect if called before compile().
+		 *
+		 * @api __toString
+		 * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.tostring
+		 */
+		public function __toString():string {
+			return base64_encode(json_encode($this->compiled));
+		}
+
+		/**
+		 * @var HTTPHeader[]
+		 */
+		protected $headers;
+
+		/**
+		 * @var string[]
+		 */
+		protected $compiled;
+	}
